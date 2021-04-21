3e63b72649f97947467f998065cc5e3ad8ab49c4
Marta Pettersson <marta.pettersson@gmail.com>
2021-04-21 14:39:42 +0200

    add more readability

--- index.php
+++ index.php
@@ -6,20 +6,20 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-$firstnamesFemale = ["Åsa", "Lotta", "Greta", "Amanda", "Marta", "Maja", "Nina", "Erika", "Deepthi", "Inna"];
-$firstnamesMale = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Björn", "Erik", "Dragan", "Özgur", "Dino"];
+$female = ["Åsa", "Lotta", "Greta", "Amanda", "Marta", "Maja", "Nina", "Erika", "Deepthi", "Inna"];
+$male = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Björn", "Erik", "Dragan", "Özgur", "Dino"];
 $lastNames = ["Johnsson", "Hedlund", "Al Hakim", "Gedda", "Basele", "Carlson", "Bernadotte", "Wonder", "Richie", "Olsson"];
 
 for ($i = 0; $i < 10; $i++) {
     $random = rand(0, 1);
-    $firstNames = $random ? $firstnamesFemale : $firstnamesMale;
+    $firstNames = $random ? $female : $male;
     $firstName = $firstNames[rand(0, 9)];
     $lastName = $lastNames[rand(0, 9)];
     $search = 'åäöÅÄÖ';
     $replace = 'aaoAAO';
-    $whitespace = " ";
-    $noSpace = "";
-    $email = strtolower(substr(strtr($firstName, $search, $replace), 0, 2) . substr(strtr(str_replace(' ', '', $lastName), $search, $replace), 0, 3)) . '@example.com';
+    $fi = substr(strtr($firstName, $search, $replace), 0, 2);
+    $las = substr(strtr(str_replace(' ', '', $lastName), $search, $replace), 0, 3);
+    $email = strtolower($fi . $las) . '@example.com';
 
     $names[] = array(
         "firstname" => $firstName,
