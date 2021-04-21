119f756717071557cd00ca38e6c5094be8cd1bcd
Marta Pettersson <marta.pettersson@gmail.com>
2021-04-21 13:54:35 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-// Steg 1 - Ange lämpliga HTTP headers
-// Läs mer här: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-
 header("Content-Type: application/json; charset=UTF-8");
 
 header("Access-Control-Allow-Origin: *");
@@ -10,41 +7,40 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-// Steg 2 - Skapa arrayer
-
-$firstNames =
-    ["Åsa", "Björn", "Marta", "Jacob", "Maja", "Erika", "Olle", "Lisah", "Freja", "Anna-Karin"];
 $lastNames =
     ["Öberg", "Åhlund", "Pettersson", "Axellie", "Thunberg", "Ahlström", "Pettersson", "Silfwer", "Lantz Giraldo", "Thorell"];
 
-// Steg 3 - Skapa 10 namn och spara dessa i en ny array
+$female = ["Åsa", "Marta", "Maja", "Erika", "Lisah", "Freja", "Anna-Karin", "Nina", "Amanda", "Inna"];
+
+$male = ["Björn", "Gustaf", "Jacob", "Erik", "Dragan", "Olle", "Dino", "Mahmud", "Axel", "Antonio"];
 
 $names = array();
 
-// 10 varv i denna loop!
 for ($i = 0; $i < 10; $i++) {
+    $random = rand(0, 1);
+    $firstNames = $random ? $female : $male;
+
+    $firstname = $firstNames[rand(0, 9)];
+    $lastname = $lastNames[rand(0, 9)];
+    $email = mb_strtolower(mb_substr($firstname, 0, 2, 'UTF-8') . mb_substr($lastname, 0, 3, 'UTF-8'), 'UTF-8') . "@example.com";
+
+    $search = array('å', 'ä', 'ö');
+    $replace = array('a', 'a', 'o');
+    $email = str_replace($search, $replace, $email);
+
     $name = array(
-        "firstname" => $firstNames[rand(0, 9)],
-        "lastname" => $lastNames[rand(0, 9)]
+        "firstname" => $firstname,
+        "lastname" => $lastname,
+        "gender" => $random ? "female" : "male",
+        "age" => rand(1, 100),
+        "email" => $email
     );
     array_push($names, $name);
 }
-
-//TEST
-// print_r($names);
-
-// Steg 4 - Konvertera PHP-arrayen($names) till JSON
 
 $json = json_encode(
     $names,
     JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
 );
 
-// json_encode — Returns the JSON representation of a value
-// Läs mer här: http://php.net/manual/en/function.json-encode.php
-
-//Steg 5 – Skicka JSON till klienten
 echo $json;
-// OBS!
-// Ingen extra data får skickas till klienten,
-// t.ex. HTML-kod
